07d2cebbe690c05a1078a80bf65798fa0ebf24f6
Simonwtaylor <Simonwtaylor93@gmail.com>
2020-02-18 21:43:07 +0000

    Worked on arrays and structs

---
+++ structs/structs.go
@@ -0,0 +1,16 @@
+package structs
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+// Perimeter ...
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+// Area ...
+func Area(rectangle Rectangle) float64 {
+	return (rectangle.Width * rectangle.Height)
+}
