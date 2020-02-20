193e3430d37297cd9fab4ae469240a1b6d0c424f
Simonwtaylor <Simonwtaylor93@gmail.com>
2020-02-20 08:55:00 +0000

    DI

---
+++ dependencyinjection/dependency_injection_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Simon")
+
+	got := buffer.String()
+	want := "Hello, Simon"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
