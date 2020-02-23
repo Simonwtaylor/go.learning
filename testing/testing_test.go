69cde95f89a8b307099efafd9d7d4e2f8360cefe
Simonwtaylor <Simonwtaylor93@gmail.com>
2020-02-23 18:14:42 +0000

    testing in progress

---
+++ testing/testing_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestRomanNumerals(t *testing.T) {
+
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 converts to I", 1, "I"},
+		{"2 converts to II", 2, "II"},
+		{"3 converts to III", 3, "III"},
+		{"4 converts to IV", 4, "IV"},
+		{"5 converts to V", 5, "V"},
+		{"9 converts to IX", 9, "IX"},
+		{"10 converts to X", 10, "X"},
+		{"14 converts to XIV", 14, "XIV"},
+		{"18 converts to XVIII", 18, "XVIII"},
+		{"20 converts to XX", 20, "XX"},
+		{"39 converts to XXXIX", 39, "XXXIX"},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
+	}
+}
